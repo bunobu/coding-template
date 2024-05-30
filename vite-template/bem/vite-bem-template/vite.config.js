a11ed96378191c487c7b340508bcf493c4a3197f
bunobu <86172592+bunobu@users.noreply.github.com>
2024-05-30 19:32:40 +0900

    :sparkles: html,css,jsの出力設定 #6

--- vite-template/bem/vite-bem-template/vite.config.js
+++ vite-template/bem/vite-bem-template/vite.config.js
@@ -1,9 +1,36 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
+import { log } from "console";
 
 export default defineConfig({
-  root: "./src",
+  root: "./src", //開発ディレクトリの設定
   build: {
+    //ファイルの出力先の設定
     outDir: "../dist",
     emptyOutDir: true,
+
+    // ファイルの出力設定
+    rollupOptions: {
+      output: {
+        // assetsファイルの設定
+        assetFileNames: (assetInfo) => {
+          if (`\.css$`) {
+            return `assets/css/[name][extname]`;
+          }
+
+          return `assets/[name][extname]`;
+        },
+
+        // jsファイルの設定
+        chunkFileNames: "assets/js/[name].js",
+        entryFileNames: "assets/js/[name].js",
+      },
+
+      input: {
+        index: resolve(__dirname, "./src/index.html"),
+        // htmlファイルを複数出力する場合はここに記載
+        hoge: resolve(__dirname, "./src/hoge.html"),
+      },
+    },
   },
 });
