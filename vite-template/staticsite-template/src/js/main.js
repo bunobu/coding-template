ea014b0d11c8496e55d0b43f10bfe46a3ef42fc9
bunobu <86172592+bunobu@users.noreply.github.com>
2024-06-20 17:24:03 +0900

    :sparkles: mobile端末の表示を担保するscriptを追加 #14

--- vite-template/staticsite-template/src/js/main.js
+++ vite-template/staticsite-template/src/js/main.js
@@ -2,3 +2,28 @@ import { tamago } from "./tamago";
 
 console.log("main");
 tamago();
+
+/**
+ * デバイス幅370px以下の場合はviewportを固定
+ * flagSize 固定を始めるデバイス幅
+ */
+(() => {
+  const viewportSwitch = () => {
+    const screenWidth = window.screen.width;
+    const meta = document.querySelector("meta[name='viewport']");
+    const flagSize = 370;
+    console.log(meta);
+
+    if (screenWidth >= flagSize) {
+      meta.setAttribute(
+        "content",
+        "width=device-width,initial-scale=1.0,viewport-fit=cover"
+      );
+    } else {
+      meta.setAttribute("content", `width=${flagSize}`);
+    }
+  };
+
+  window.addEventListener("resize", viewportSwitch);
+  viewportSwitch();
+})();
